31dbd836988d4deb8dcc4ec29ad15280c1b439f8
vincent911009 <97635812+vincent911009@users.noreply.github.com>
2022-12-06 04:19:58 -0500

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from basketball_reference_scraper.teams import get_roster, get_team_stats, get_opp_stats, get_roster_stats, get_team_misc
-from basketball_reference_scraper.players import get_stats, get_game_logs, get_player_headshot
-from basketball_reference_scraper.seasons import get_schedule, get_standings
-from basketball_reference_scraper.box_scores import get_box_scores
-
-a = get_roster_stats('GSW',2023)
-print(a['3P%'])
